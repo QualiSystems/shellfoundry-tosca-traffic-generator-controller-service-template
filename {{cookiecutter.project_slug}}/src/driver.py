1e9ea5f7ce2b6fdc187a02ac4ef54da2b0138996
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-03-04 18:16:04 +0200

    Added basic controller commands

--- {{cookiecutter.project_slug}}/src/driver.py
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -4,7 +4,7 @@ from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCom
 #from data_model import *  # run 'shellfoundry generate' to generate data model classes
 
 
-class {{cookiecutter.driver_name}} (ResourceDriverInterface):
+class {{cookiecutter.driver_name}}ResourceDriverInterface):
 
     def __init__(self):
         """
@@ -24,5 +24,42 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         """
         Destroy the driver session, this function is called everytime a driver instance is destroyed
         This is a good place to close any open sessions, finish writing to log files
+        """
+        pass
+
+
+    def load_configuration(self, context, config_file_location):
+        """
+        Load configuration file
+        :param context:
+        :param config_file_location:
+        :return:
+        """
+        pass
+
+    def start_traffic(self, context, blocking):
+        """
+        Start traffic
+        :param context: the context the command runs on
+        :type context: cloudshell.shell.core.driver_context.ResourceRemoteCommandContext
+        :param blocking:
+        """
+        pass
+
+    def stop_traffic(self, context):
+        """
+        Stop traffic
+        :param context: the context the command runs on
+        :type context: cloudshell.shell.core.driver_context.ResourceRemoteCommandContext
+        """
+        pass
+
+    def get_statistics(self, context, view_name, output_type):
+        """
+        Get statistics
+        :param context:
+        :param view_name:
+        :param output_type:
+        :return:
         """
         pass
