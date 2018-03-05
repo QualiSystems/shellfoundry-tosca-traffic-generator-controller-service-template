238a045dfa1d8c692c95f6502c4f7956453cd9ac
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-03-05 14:12:00 +0200

    Fixed driver methods

--- {{cookiecutter.project_slug}}/src/driver.py
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -1,32 +1,12 @@
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadResource, \
-    AutoLoadAttribute, AutoLoadDetails, CancellationContext
-#from data_model import *  # run 'shellfoundry generate' to generate data model classes
 
-
-class {{cookiecutter.driver_name}}ResourceDriverInterface):
+class {{cookiecutter.driver_name}}(ResourceDriverInterface):
 
     def __init__(self):
         """
         ctor must be without arguments, it is created with reflection at run time
         """
         pass
-
-    def initialize(self, context):
-        """
-        Initialize the driver session, this function is called everytime a new instance of the driver is created
-        This is a good place to load and cache the driver configuration, initiate sessions etc.
-        :param InitCommandContext context: the context the command runs on
-        """
-        pass
-
-    def cleanup(self):
-        """
-        Destroy the driver session, this function is called everytime a driver instance is destroyed
-        This is a good place to close any open sessions, finish writing to log files
-        """
-        pass
-
 
     def load_configuration(self, context, config_file_location):
         """
@@ -60,6 +40,14 @@ class {{cookiecutter.driver_name}}ResourceDriverInterface):
         :param context:
         :param view_name:
         :param output_type:
+        :return:
+        """
+        pass
+
+    def cleanup_reservation(self, context):
+        """
+        Clear reservation when it ends
+        :param context:
         :return:
         """
         pass
